1b74f00779d21864ae726970f8717dc20ad74218
Saaappi <alex.metz1024@gmail.com>
2021-06-07 00:38:25 -0500

    The number of usuable mounts is now accurate.

--- MountCountEnhanced.lua
+++ MountCountEnhanced.lua
@@ -12,9 +12,22 @@
 ]]--
 local addonName, t = ...;
 local e = CreateFrame("Frame");
+local useErrors = {
+	"To use this mount in the current area you must be underwater.",
+	"You can't use that here.",
+};
 
 -- Event Registrations
 e:RegisterEvent("ADDON_LOADED");
+
+-- Functions
+local function CheckForUseError(useError)
+	for _, err in ipairs(useErrors) do
+		if useError == err then
+			return true;
+		end
+	end
+end
 
 -- Logic
 e:SetScript("OnEvent", function(self, event, addon)
@@ -25,24 +38,27 @@ e:SetScript("OnEvent", function(self, event, addon)
 		MountJournal.MountCount:SetPoint("TOPLEFT", MountJournal, "TOPLEFT", 70, -25);
 		
 		-- Get mount information.
-		local collected = 0;
-		local uncollected = 0;
-		local unusable = 0;
+		local numMountsCollectedOnAccount = 0; -- This includes all mounts collected on the account, regardless of faction.
+		local numMountsUncollectedOnAccount = 0; -- Self explanatory. Get to work!
+		local numMountsUsableOnAccount = 0; -- The number of mounts usable to the character.
 		local mountIDs = C_MountJournal.GetMountIDs();
-		for i, mountID in ipairs(mountIDs) do
-			local _, _, _, _, isUsable, _, _, _, _, hideOnChar, isCollected = C_MountJournal.GetMountInfoByID(mountID);
-			if (isCollected and hideOnChar == true) then
-				unusable = unusable + 1;
-			elseif (isCollected ~= true and hideOnChar ~= true) then
-				uncollected = uncollected + 1;
-			elseif (isCollected and hideOnChar ~= true) then
-				collected = collected + 1;
+		for index, mountID in ipairs(mountIDs) do
+			local name, _, _, _, _, _, _, _, _, hideOnChar, isCollected = C_MountJournal.GetMountInfoByID(mountID);
+			local isUsable, useError = C_MountJournal.GetMountUsabilityByID(mountID, false);
+			if ((isCollected and isUsable) or (isCollected and CheckForUseError(useError))) then
+				numMountsUsableOnAccount = numMountsUsableOnAccount + 1;
+			end
+			if (isCollected and hideOnChar ~= true) then
+				numMountsCollectedOnAccount = numMountsCollectedOnAccount + 1;
+			end
+			if (isCollected ~= true and hideOnChar ~= true) then
+				numMountsUncollectedOnAccount = numMountsUncollectedOnAccount + 1;
 			end
 		end
 		
-		MountJournal.MountCount.Label:SetText("Collected: |cffFFFFFF" .. collected .. "|r\n" ..
-		"Uncollected: |cffFFFFFF" .. uncollected .. "|r\n" ..
-		"Usable: |cffFFFFFF" .. (collected-unusable) .. "|r");
+		MountJournal.MountCount.Label:SetText("Collected: |cffFFFFFF" .. numMountsCollectedOnAccount .. "|r\n" ..
+		"Uncollected: |cffFFFFFF" .. numMountsUncollectedOnAccount .. "|r\n" ..
+		"Usable: |cffFFFFFF" .. numMountsUsableOnAccount .. "|r");
 		MountJournal.MountCount.Count:Hide();
 	end
 end);
